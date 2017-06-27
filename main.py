47a599752cfefbc2dc223b9eed978f8543730b8a
yekki <gary.niu@gmail.com>
2017-06-27 17:17:30 +0800

    update

--- main.py
+++ main.py
@@ -1,76 +1,82 @@
 from pathlib import Path
 import json, os
 
-SOURCE_PATH = '/Users/gniu/(lyh)罗永浩干货日记'
+SOURCE_PATH = '/Users/gniu//(lyh)罗永浩干货日记'
 DEST_PATH = '/Users/gniu/Temp.localized/lyh'
-ALBUM_ABBR = 'lyh'
 
 meta = dict()
-
 meta['name'] = '罗永浩干货日记'
 meta['author'] = '罗永浩'
 meta['abbr'] = 'lyh'
+meta['seasons'] = [dict(no='1', months=list())]
 
 
-def get_files_from_dir(root, endswith):
-    findings = list()
-    for root, dirs, files in os.walk(root):
-        for file in files:
-            if file.endswith(endswith):
-                findings.append(os.path.join(root, file))
+class AlbumParser:
+    def __init__(self, root, title, author, abbreviation, year, season_no):
+        self._title = title
+        self._author = author
+        self._abbreviation = abbreviation
+        self._root = root
+        self._season_no = season_no
+        self._year = year
+        self._files = list()
 
-    return findings
+        for root, dirs, files in os.walk(self._root):
+            for file in files:
+                self._files.append(Path(os.path.join(root, file)))
 
-def get_months_zh(root, year):
-    files = get_files_from_dir(root, '.mp3')
-    months = set([Path(f).stem[0:5].replace('lyh', '') for f in files])
-    months_zh = set([f'{year}年{mon}月' for mon in months])
-    return months_zh
+    def files(self, prefix=None, suffix=None):
 
-def get_months(root):
-    files = get_files_from_dir(root, '.mp3')
-    return set([Path(f).stem[0:5].replace('lyh', '') for f in files])
+        if suffix and prefix:
+            return set(filter(lambda x: x.suffix == suffix and x.stem.startswith(prefix), self._files))
+        elif prefix:
+            return set(filter(lambda x: x.stem.startswith(prefix), self._files))
+        elif suffix:
+            return set(filter(lambda x: x.suffix == suffix, self._files))
+        else:
+            return self._files
 
+    def months(self):
+        mp3s = self.files(suffix='.mp3')
+        func = lambda x: self._year + '年' + x.stem[0:len(self._abbreviation) + 2].replace(self._abbreviation, '') + '月'
 
-def get_days_in_month(root, month):
-    mp3_files = set(get_files_from_dir(root, '.mp3'))
+        return set(map(func, mp3s))
 
-    return filter(lambda x: Path(x).stem.startswith(meta['abbr'] + month), mp3_files)
+    def episodes(self):
+        pass
 
+    def episodes_in_month(self, month):
+        days = set([f.stem for f in self.files(suffix='.mp3', prefix=f'{self._abbreviation}{month}')])
+        episodes = dict()
+        for d in days:
+            index = len(self._abbreviation)
+            episodes[d[index:]] = self.media(d)
 
-def generate_meta():
-    all_days = set([f.stem.replace(meta['abbr'], '')[0:4] for f in Path(SOURCE_PATH).glob('**/*.mp3') if
-                    f.is_file() and f.stem.startswith(ALBUM_ABBR)])
-    months = dict.fromkeys(set(m[0:2] for m in all_days))
-    season = dict(no='01', months=months)
-    for m in months:
-        days = list(filter(lambda x: x.startswith(m), all_days))
-        episodes = list()
-        for day in days:
-            pics = [f.name for f in Path(SOURCE_PATH).glob('**/*.jpg') if
-                    f.is_file() and f.stem.startswith(meta['abbr'] + day)]
-            mp3s = [f.name for f in Path(SOURCE_PATH).glob('**/*.mp3') if
-                    f.is_file() and f.stem.startswith(meta['abbr'] + day)]
+        return episodes
 
-            title = min(pics, key=len)
-            title = title.replace(day, '')
-            if title[-1].isdigit():
-                title = title[:-1]
-            episode = dict(day=day, title=title, mp3s=mp3s, pics=pics)
-            episodes.append(episode)
-        season['months'][m] = episodes
+    def media(self, prefix):
+        mp3s = list(self.files(prefix=prefix, suffix='.mp3'))
+        pics = list(self.files(prefix=prefix, suffix='.jpg'))
 
-    meta['seasons'] = list()
-    meta['seasons'].append(season)
+        title = pics[0].stem
+        title = title[title.index('第'):]
 
-    data = json.dumps(meta, ensure_ascii=False, indent=4)
+        if title[-1].isdigit():
+            title = title[:-1]
 
-    with open('meta.json', 'w', encoding='utf-8') as fp:
-        fp.write(data)
+        return dict(title=title, mp3s=[f.name for f in mp3s], pics=[f.name for f in pics])
+
+    def dump_to_file(data, output='meta.json'):
+        data = json.dumps(meta, ensure_ascii=False, indent=4)
+
+        with open('meta.json', 'w', encoding='utf-8') as fp:
+            fp.write(data)
 
 
 def main():
-    mons = get_months_zh(SOURCE_PATH, '2017')
-    print(mons.pop()[-3:-1])
+    album = AlbumParser(SOURCE_PATH, '罗永浩干货日记', '罗永浩', 'lyh', '2017', '01')
+    print(album.episodes_in_month('05'))
+
+
 if __name__ == '__main__':
     main()
